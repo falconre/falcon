8fe8ca36f1412bb4425580601ae3e0915f48bb07
Alex Eubanks <endeavor@rainbowsandpwnies.com>
2017-10-06 23:31:04 -0400

    fixing up .ci.sh

--- .ci.sh
+++ .ci.sh
@@ -3,6 +3,5 @@
 source ~/.profile
 cd /falcon
 cargo test
-apt-get install -y cmake libssl-dev
-cargo install cargo-tarpaulin
+curl -sL https://github.com/xd009642/tarpaulin/releases/download/0.5.0/cargo-tarpaulin-0.5.0-travis.tar.gz | tar xvz -C $HOME/.cargo/bin
 cargo tarpaulin --ciserver travis-ci --coveralls $1
