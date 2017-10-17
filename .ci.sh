7195953bde30985a79c823be66c36e0288522192
endeavor <endeavor@rainbowsandpwnies.com>
2017-10-16 22:22:03 -0400

    run both regular and ignored tests in travis

--- .ci.sh
+++ .ci.sh
@@ -3,5 +3,6 @@
 source ~/.profile
 cd /falcon
 cargo test
+cargo test --ignored
 curl -sL https://github.com/xd009642/tarpaulin/releases/download/0.5.0/cargo-tarpaulin-0.5.0-travis.tar.gz | tar xvz -C $HOME/.cargo/bin
-cargo tarpaulin --ciserver travis-ci --coveralls $1
+cargo tarpaulin -i --ciserver travis-ci --coveralls $1
