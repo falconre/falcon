0bc248bb46d7f780e31162927b92e89e8e370a32
endeavor <endeavor@rainbowsandpwnies.com>
2017-10-16 22:24:52 -0400

    updating ci script

--- .ci.sh
+++ .ci.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-source ~/.profile
-cd /falcon
-cargo test
-cargo test --ignored
-curl -sL https://github.com/xd009642/tarpaulin/releases/download/0.5.0/cargo-tarpaulin-0.5.0-travis.tar.gz | tar xvz -C $HOME/.cargo/bin
-cargo tarpaulin -i --ciserver travis-ci --coveralls $1
+source ~/.profile && \
+cd /falcon && \
+cargo test && \
+cargo test -- --ignored && \
+curl -sL https://github.com/xd009642/tarpaulin/releases/download/0.5.0/cargo-tarpaulin-0.5.0-travis.tar.gz | tar xvz -C $HOME/.cargo/bin && \
+cargo tarpaulin -i --ignore-tests --no-count --ciserver travis-ci --coveralls $1
