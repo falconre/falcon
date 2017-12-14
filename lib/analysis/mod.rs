421dc38fec736b7506e309cec24f23ac4dd831cb
Alex Eubanks <endeavor@rainbowsandpwnies.com>
2017-12-13 20:17:24 -0500

    re-expose analysis::def_use

--- lib/analysis/mod.rs
+++ lib/analysis/mod.rs
@@ -8,7 +8,7 @@ mod location_set;
 mod reaching_definitions;
 mod use_def;
 
-// pub use self::def_use::def_use;
+pub use self::def_use::def_use;
 pub use self::location_set::LocationSet;
 pub use self::reaching_definitions::reaching_definitions;
 pub use self::use_def::use_def;
