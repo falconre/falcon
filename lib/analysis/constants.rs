13c7ef09e903d7fdecc957fca2fc3365df4ecaa3
Alex Eubanks <endeavor@rainbowsandpwnies.com>
2018-02-11 14:17:04 -0500

    fix up constants analysis

--- lib/analysis/constants.rs
+++ lib/analysis/constants.rs
@@ -1,4 +1,11 @@
 //! A very simple, and fast, constant propagation
+//!
+//! Each location has the known constant values for all variables before
+//! execution of that location.
+//!
+//! Calling Constants::eval() uses the known constant values to replace scalars,
+//! and then attempts to evaluate the expression to an `il::Constant`.
+
 
 use analysis::fixed_point;
 use error::*;
@@ -11,13 +18,77 @@ use std::cmp::{Ordering, PartialOrd};
 /// Compute constants for the given function
 pub fn constants<'r>(function: &'r il::Function)
 -> Result<HashMap<il::RefProgramLocation<'r>, Constants>> {
-    fixed_point::fixed_point_forward(ConstantsAnalysis{}, function)
+    let constants = fixed_point::fixed_point_forward(ConstantsAnalysis{}, function)?;
+
+    // we're now going to remap constants, so each position holds the values of
+    // constants immediately preceeding it.
+
+    let mut result = HashMap::new();
+
+    for (location, _) in &constants {
+        result.insert(location.clone(),
+            location.backward()?
+                .into_iter()
+                .fold(Constants::new(), |c, location|
+                    c.join(&constants[&location])));
+    }
+
+    Ok(result)
+}
+
+
+#[allow(dead_code)] // Bottom is never used
+#[derive(Clone, Debug, PartialEq)]
+enum Constant {
+    Top,
+    Constant(il::Constant),
+    Bottom
+}
+
+
+impl Constant {
+    fn get(&self) -> Option<&il::Constant> {
+        match *self {
+            Constant::Constant(ref constant) => Some(constant),
+            Constant::Top |
+            Constant::Bottom => None
+        }
+    }
+}
+
+
+impl PartialOrd for Constant {
+    fn partial_cmp(&self, other: &Constant) -> Option<Ordering> {
+        match *self {
+            Constant::Top => match *other {
+                Constant::Top => Some(Ordering::Equal),
+                Constant::Constant(_) |
+                Constant::Bottom => Some(Ordering::Greater)
+            },
+            Constant::Constant(ref lc) => match *other {
+                Constant::Top => Some(Ordering::Less),
+                Constant::Constant(ref rc) =>
+                    if lc == rc {
+                        Some(Ordering::Equal)
+                    }
+                    else {
+                        None
+                    },
+                Constant::Bottom => Some(Ordering::Greater),
+            },
+            Constant::Bottom => match *other {
+                Constant::Top |
+                Constant::Constant(_) => Some(Ordering::Less),
+                Constant::Bottom => Some(Ordering::Equal)
+            }
+        }
+    }
 }
 
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct Constants {
-    constants: HashMap<il::Scalar, il::Constant>
+    constants: HashMap<il::Scalar, Constant>
 }
 
 
@@ -28,7 +99,7 @@ impl PartialOrd for Constants {
             for (ls, lc) in self.constants.iter() {
                 if !other.constants
                     .get(ls)
-                    .map(|rc| rc >= lc)
+                    .map(|rc| lc <= rc)
                     .unwrap_or(false) {
                     return None;
                 }
@@ -39,18 +110,40 @@ impl PartialOrd for Constants {
             for (ls, lc) in other.constants.iter() {
                 if !self.constants
                     .get(ls)
-                    .map(|rc| rc >= lc)
+                    .map(|rc| lc <= rc)
                     .unwrap_or(false) {
                     return None;
                 }
             }
-            Some(Ordering::Less)
-        }
-        else if self.constants == other.constants {
-            Some(Ordering::Equal)
+            Some(Ordering::Greater)
         }
         else {
-            None
+            let mut order = Ordering::Equal;
+            for (ls, lc) in &self.constants {
+                match other.constants.get(ls) {
+                    Some(rc) =>
+                        if lc < rc {
+                            if order <= Ordering::Equal {
+                                order = Ordering::Less;
+                            }
+                            else {
+                                return None;
+                            }
+                        }
+                        else if lc > rc {
+                            if order >= Ordering::Equal {
+                                order = Ordering::Greater;
+                            }
+                            else {
+                                return None;
+                            }
+                        },
+                    None => {
+                        return None;
+                    }
+                }
+            }
+            Some(order)
         }
     }
 }
@@ -64,16 +157,19 @@ impl Constants {
     }
 
     pub fn scalar(&self, scalar: &il::Scalar) -> Option<&il::Constant> {
-        self.constants.get(scalar)
+        self.constants
+            .get(scalar)
+            .map(|constant| constant.get())
+            .unwrap_or(None)
     }
 
-    pub fn set_scalar(&mut self, scalar: il::Scalar, constant: Option<il::Constant>) {
-        if constant.is_none() {
-            self.constants.remove(&scalar);
-        }
-        else {
-            self.constants.insert(scalar, constant.unwrap());
-        }
+    fn set_scalar(&mut self, scalar: il::Scalar, constant: Constant) {
+        self.constants.insert(scalar, constant);
+    }
+
+    fn top(&mut self) {
+        self.constants.iter_mut()
+            .for_each(|(_, constant)| *constant = Constant::Top);
     }
 
     pub fn eval(&self, expression: &il::Expression) -> Option<il::Constant> {
@@ -93,19 +189,15 @@ impl Constants {
         eval(&expression).ok()
     }
 
-    // TODO: This is an easy target for optiimization
-    pub fn join(self, other: &Constants) -> Constants {
+    fn join(self, other: &Constants) -> Constants {
         let mut result = self.clone();
         for (scalar, constant) in other.constants.iter() {
-            match self.scalar(scalar) {
+            match self.constants.get(scalar) {
                 Some(c) => 
-                if c != constant {
-                    result.set_scalar(scalar.clone(), None);
-                }
-                else {
-                    result.set_scalar(scalar.clone(), None);
-                }
-                None => result.set_scalar(scalar.clone(), Some(constant.clone()))
+                    if c != constant {
+                        result.set_scalar(scalar.clone(), Constant::Top);
+                    },
+                None => result.set_scalar(scalar.clone(), constant.clone())
             }
         }
         result
@@ -129,17 +221,23 @@ impl<'r> fixed_point::FixedPointAnalysis<'r, Constants> for ConstantsAnalysis {
         let state = match location.instruction() {
             Some(instruction) => match *instruction.operation() {
                 il::Operation::Assign { ref dst, ref src } => {
-                    let constant = state.eval(src);
+                    let constant =
+                        state.eval(src)
+                            .map(|constant| Constant::Constant(constant))
+                            .unwrap_or(Constant::Top);
                     state.set_scalar(dst.clone(), constant);
                     state
                 },
                 il::Operation::Load { ref dst, .. } => {
-                    state.set_scalar(dst.clone(), None);
+                    state.set_scalar(dst.clone(), Constant::Top);
                     state
-                }
+                },
                 il::Operation::Store { .. } |
                 il::Operation::Branch { .. } |
-                il::Operation::Raise { .. } => Constants::new()
+                il::Operation::Raise { .. } => {
+                    state.top();
+                    state
+                }
             },
             None => state
         };
