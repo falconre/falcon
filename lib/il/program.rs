c787281cb1911c71cbbe1f55134c9874e5892080
Alex Eubanks <endeavor@rainbowsandpwnies.com>
2018-07-24 21:34:40 -0400

    0.3.2 version bump

--- lib/il/program.rs
+++ lib/il/program.rs
@@ -3,12 +3,13 @@
 use il::*;
 use std::collections::BTreeMap;
 use std::fmt;
+use RC;
 
 /// A representation of a program by `il::Function`
 #[derive(Clone, Debug, Deserialize, Hash, Serialize)]
 pub struct Program {
     // Mapping of function indices (not addresses) to `Function`.
-    functions: BTreeMap<u64, Function>,
+    functions: BTreeMap<u64, RC<Function>>,
     // The next index to assign to a function when added to the program.
     next_index: u64
 }
@@ -46,8 +47,11 @@ impl Program {
 
 
     /// Get the underlying BTreeMap holding all `Function` for this `Program`.
-    pub fn functions_map(&self) -> &BTreeMap<u64, Function> {
-        &self.functions
+    pub fn functions_map(&self) -> BTreeMap<u64, &Function> {
+        self.functions
+            .iter()
+            .map(|(index, function)| (*index, function.as_ref()))
+            .collect::<BTreeMap<u64, &Function>>()
     }
 
 
@@ -63,21 +67,12 @@ impl Program {
     }
 
 
-    /// Returns a `Rc<Function>` by it's index.
-    pub fn function_rc(&self, index: u64) -> Option<Function> {
-        match self.functions.get(&index) {
-            Some(f) => Some(f.clone()),
-            None => None
-        }
-    }
-
-
     /// Add a `Function` to the `Program`.
     ///
     /// This will also assign an index to the `Function`.
     pub fn add_function(&mut self, mut function: Function) {
         function.set_index(Some(self.next_index));
-        self.functions.insert(self.next_index, function);
+        self.functions.insert(self.next_index, RC::new(function));
         self.next_index += 1;
     }
 }
