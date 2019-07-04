78de4d87f219092595c06b2b4a70727286e5e848
endeavor <endeavor@rainbowsandpwnies.com>
2019-07-04 14:34:31 -0400

    actually remove cargo tarpaulin, we don't use it

--- .ci.sh
+++ .ci.sh
@@ -4,6 +4,4 @@ source ~/.profile && \
 cd /falcon && \
 cargo test && \
 cargo test -- --ignored && \
-cargo test --features thread_safe&& \
-cargo install cargo-tarpaulin
-cargo tarpaulin -i --ignore-tests --no-count --ciserver travis-ci --coveralls $1
+cargo test --features thread_safe
