1ac88408638455bda38eb23ecf745835dece1f6d
endeavor <endeavor@rainbowsandpwnies.com>
2020-02-16 18:03:38 -0500

    fix ror

--- lib/translator/x86/test.rs
+++ lib/translator/x86/test.rs
@@ -239,3 +239,24 @@ fn rol() {
         &il::const_(0x7fdffffffa000001, 64)
     );
 }
+
+
+#[test]
+fn ror() {
+    // ror r8, 0x11
+    // nop
+    let bytes: Vec<u8> = vec![0x49, 0xc1, 0xc8, 0x11, 0x90];
+
+    let driver = init_amd64_driver(
+        bytes.clone(),
+        vec![("r8", il::const_(0x7fdfffffed200001, 64))],
+        Memory::new(Endian::Little),
+    );
+
+    let driver = step_to(driver, 0x4);
+
+    assert_eq!(
+        driver.state().get_scalar("r8").unwrap(),
+        &il::const_(0xbfeffffff690, 64)
+    );
+}
