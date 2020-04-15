34f6727dad225efdb03cfe4ac9062482fbbf5b77
anon8675309 <adam@dc949.org>
2020-04-15 16:00:13 -0500

    Added scripts to get set up outside of docker (#72)
    * Added scripts to get set up outside of docker (Vagrant, manually created VM, physical machine, etc.)
    
    * Attempting to fix the travis CI failure
    
    Co-authored-by: vagrant <vagrant@debian>
    Co-authored-by: anon <anon@localhost>

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+./dependencies.sh
+if [ $? -ne 0 ]; then
+	echo "Failed to install dependencies"
+	exit 1
+fi
+
+. $HOME/.cargo/env
+git clone https://github.com/falconre/falcon
+cd falcon
+cargo build
+cargo new falcontest
+cd falcontest
+echo 'falcon = { path = ".." }' >> Cargo.toml
+echo 'extern crate falcon;
+
+use falcon::loader::Elf;
+use falcon::loader::Loader;
+use std::path::Path;
+
+fn main() {
+    let elf = Elf::from_file(Path::new("/bin/sh")).unwrap();
+    for function in elf.program().unwrap().functions() {
+        for block in function.blocks() {
+            println!("Block {} in Function {:x}", block.index(), function.address());
+            println!("{}", block);
+        }
+    }
+}' > src/main.rs
+cargo build
+cargo run
+
+exit $?
