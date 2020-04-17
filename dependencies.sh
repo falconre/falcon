d5fb7e45d14d4ab5bbdf6e4d5371284fa2bc4be5
anon8675309 <adam@dc949.org>
2020-04-17 02:17:14 -0500

    Remove docker network dependency (#73)
    * Added scripts to get set up outside of docker (Vagrant, manually created VM, physical machine, etc.)
    
    * Removed dependency on network connectivity from within Docker; made dependencies.sh compatible with Debian (which doesn't have sudo by default)
    
    * Attempting to fix the travis CI failure
    
    * Sneaking in a little improvement to the test program to make it a cooler looking example
    
    * Added support for Buster
    
    Co-authored-by: vagrant <vagrant@debian>
    Co-authored-by: anon <anon@localhost>

--- dependencies.sh
+++ dependencies.sh
@@ -1,28 +1,34 @@
 #!/bin/sh
-# Script assumes the ability to run sudo without a password
+if [ `whoami` != "root" ]; then
+	echo "$0 must be run as root"
+	exit 1
+fi
 
 grep 'ID=debian' /etc/os-release
 if [ $? -eq 0 ]; then
-	grep 'VERSION_CODENAME=stretch' /etc/os-release
-	if [ $? -eq 0 ]; then
-		sudo DEBIAN_FRONTEND=noninteractive apt-get update
-		sudo DEBIAN_FRONTEND=noninteractive apt-get -y dist-upgrade
-		sudo DEBIAN_FRONTEND=noninteractive apt-get -y install curl gnupg2 apt-transport-https
-		echo "deb http://apt.llvm.org/stretch/ llvm-toolchain-stretch-4.0 main" | sudo tee -a /etc/apt/sources.list
-		echo "deb-src http://apt.llvm.org/stretch/ llvm-toolchain-stretch-4.0 main" | sudo tee -a /etc/apt/sources.list
-		curl http://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
-		sudo DEBIAN_FRONTEND=noninteractive apt-get update
-		sudo DEBIAN_FRONTEND=noninteractive apt-get -y install build-essential clang-4.0 curl llvm-4.0-dev \
+	# Commands that are common to all Debian distros
+	export DEBIAN_FRONTEND=noninteractive
+	apt-get update
+	apt-get -y dist-upgrade
+	apt-get -y install curl gnupg2 apt-transport-https
+
+	# Release-specific commands
+	codename=`grep 'VERSION_CODENAME=' /etc/os-release | sed 's/.*=//g'`
+	if [ "$codename" = "stretch" ]; then
+		echo "deb http://apt.llvm.org/stretch/ llvm-toolchain-stretch-4.0 main" | tee -a /etc/apt/sources.list
+		echo "deb-src http://apt.llvm.org/stretch/ llvm-toolchain-stretch-4.0 main" | tee -a /etc/apt/sources.list
+		curl http://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
+		apt-get update
+		apt-get -y install build-essential clang-4.0 curl llvm-4.0-dev \
 			libcapstone3 libcapstone-dev libclang-4.0-dev pkg-config
-		sudo DEBIAN_FRONTEND=noninteractive apt-get clean
-		curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > rustup.sh
-		#curl https://sh.rustup.rs -sSf > /tmp/install.sh
-		chmod +x rustup.sh
-		./rustup.sh -y
+	elif [ "$codename" = "buster" ]; then
+		apt-get -y install build-essential clang curl llvm-dev \
+			libcapstone3 libcapstone-dev libclang-dev pkg-config
 	else
 		echo "Unsupported version of Debian"
 		exit 1
 	fi
+	apt-get clean
 else
 	echo "Unsupported Distro"
 	exit 1
--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
-./dependencies.sh
+sudo ./dependencies.sh
 if [ $? -ne 0 ]; then
 	echo "Failed to install dependencies"
+	exit 1
+fi
+./get_rust.sh
+if [ $? -ne 0 ]; then
+	echo "Failed to install rust"
 	exit 1
 fi
 
@@ -22,7 +27,8 @@ fn main() {
     let elf = Elf::from_file(Path::new("/bin/sh")).unwrap();
     for function in elf.program().unwrap().functions() {
         for block in function.blocks() {
-            println!("Block {} in Function {:x}", block.index(), function.address());
+            let f = elf.program().unwrap().function_by_address(function.address()).unwrap().name();
+            println!("Block {} in Function {:x} ({})", block.index(), function.address(), f);
             println!("{}", block);
         }
     }
